ab056e5db51188afc1581f2ce4b947b720446f89
Xaver Schüll <botacode@gmail.com>
2023-05-03 10:56:04 +0200

    Created ItemsDisplay component for Fish Tank page

--- components/ItemsDisplay/index.js
+++ components/ItemsDisplay/index.js
@@ -1,47 +1,54 @@
 import styled from "styled-components";
-import Image from "next/image";
+
+import Card from "../Card";
 
 const StyledItemContainer = styled.div`
   width: 100%;
 `;
 
 const StyledItemsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   list-style: none;
   padding-left: 0;
+  width: 100%;
+  gap: 1rem;
 `;
 
 const StyledItemListItem = styled.li`
-  border: 1px solid black;
-  border-radius: 20px;
-  padding: 0 0 1rem 1rem;
-  margin-top: 1rem;
+  position: relative;
+  width: 90%;
+  justify-self: center;
+  align-self: center;
 `;
 
 const TYPE_HEADINGS = {
   fish: "",
+  furniture: "Variants",
 };
 
-export default function ItemsDisplay({ type, fish, furniture }) {
-  const heading = TYPE_HEADINGS[type];
+export default function ItemsDisplay({ list }) {
+  const heading = TYPE_HEADINGS[list[0].type];
 
   return (
     <StyledItemContainer>
       {heading && <h3>{heading}</h3>}
-      {/* <StyledItemsList>
-        {furniture.variants.map((variant) => {
+      <StyledItemsList>
+        {list.map((item) => {
           return (
-            <StyledItemListItem key={variant.variant}>
-              <h4>{variant.variant}</h4>
-              <Image
-                src={variant.imageSource}
-                alt="dummy alt"
-                width={100}
-                height={100}
+            <StyledItemListItem key={item.slug}>
+              <Card
+                name={item.name}
+                type={item.type}
+                slug={item.slug}
+                iconSource={item.iconSource}
+                variant="fishTank"
               />
             </StyledItemListItem>
           );
         })}
-      </StyledItemsList> */}
+      </StyledItemsList>
     </StyledItemContainer>
   );
 }
