133404bdc6ebfa395ac80ab180ec8fad502edcba
Xaver Schüll <botacode@gmail.com>
2023-05-10 17:36:19 +0200

    Some global styling

--- styles.js
+++ styles.js
@@ -9,9 +9,12 @@ export default createGlobalStyle`
 
   body {
     font-family: 'Arial Rounded MT Bold', monospace;
+    color: #2B0B00;
+    background-color: #F9F6E6;
     margin: 0 auto;
     display: flex;
     flex-direction:column;
     align-items: center;
+    min-height: 120vh;
   }
 `;
