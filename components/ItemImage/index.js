1a79fe82a7f347c9ba07c3c158be724564a0277e
Xaver Schüll <botacode@gmail.com>
2023-05-11 13:08:05 +0200

    Optimised image styling for larger screens

--- components/ItemImage/index.js
+++ components/ItemImage/index.js
@@ -2,37 +2,40 @@ import styled, { css } from "styled-components";
 
 const StyledImage = styled.img`
   width: 100%;
-  position: absolute;
-  bottom: -30px;
 
-  ${({ variant }) =>
-    variant === "villager" &&
-    css`
-      left: -100px;
-      bottom: -100px;
-    `};
+  @media (max-width: 533px) {
+    position: absolute;
+    bottom: -30px;
 
-  ${({ variant }) =>
-    variant === "bug" &&
-    css`
-      left: -100px;
-      bottom: 60px;
-    `};
+    ${({ variant }) =>
+      variant === "villager" &&
+      css`
+        left: -100px;
+        bottom: -100px;
+      `};
 
-  ${({ variant }) =>
-    variant === "fish" &&
-    css`
-      left: -80px;
-      bottom: 80px;
-      transform: scaleX(-1);
-    `};
+    ${({ variant }) =>
+      variant === "bug" &&
+      css`
+        left: -100px;
+        bottom: 60px;
+      `};
 
-  ${({ variant }) =>
-    variant === "furniture" &&
-    css`
-      left: -100px;
-      transform: scaleX(-1);
-    `}
+    ${({ variant }) =>
+      variant === "fish" &&
+      css`
+        left: -80px;
+        bottom: 80px;
+        transform: scaleX(-1);
+      `};
+
+    ${({ variant }) =>
+      variant === "furniture" &&
+      css`
+        left: -100px;
+        transform: scaleX(-1);
+      `}
+  }
 `;
 
 export default function ItemImage({ item }) {
