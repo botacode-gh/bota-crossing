13f42a1ba021d251994687076da09f28399ed7af
Xaver Schüll <botacode@gmail.com>
2023-05-11 12:47:00 +0200

    Made neat functions out of messy ternary statements

--- components/BugDescription/index.js
+++ components/BugDescription/index.js
@@ -1,47 +1,20 @@
+import {
+  getLocationText,
+  getMonthsText,
+  getRarityText,
+  getTimeText,
+} from "@/lib/utils";
 import DescriptionBox from "../DescriptionBox";
 
 export default function BugDescription({ bug }) {
   const { image_url, rarity, location } = bug;
   const { months, time } = bug.north.availability_array[0];
 
-  const rarityText =
-    rarity === ""
-      ? "common "
-      : rarity === "Unknown"
-      ? null
-      : `${rarity.toLowerCase()} `;
-
-  const locationText =
-    location === "From hitting rocks"
-      ? "By hitting rocks"
-      : location === "On trees (any kind)"
-      ? "On trees of any kind"
-      : location === "Shaking trees (hardwood and cedar)"
-      ? "By shaking hardwood & cedar trees"
-      : location === "On trees (hardwood and cedar)"
-      ? "On hardwood & cedar trees"
-      : location === "Flying"
-      ? "Flying around"
-      : location === "Pushing snowballs"
-      ? "Pushing snowballs around"
-      : location === "On villagers"
-      ? "On villagers 🤢"
-      : location === "Shaking trees"
-      ? "By shaking trees"
-      : location === "Shaking non-fruit hardwood trees or cedar trees"
-      ? "By shaking non-fruit hardwood trees or cedar trees"
-      : location;
-
-  const monthsText =
-    months === "All year"
-      ? " all\u2011year"
-      : months.includes("; ")
-      ? ` from ${months.replace(/; /g, " & ")}`
-      : ` from ${months}`;
-
-  const timeText = time === "All day" ? "all\u2011day" : time;
-
   const article = /[aeiou]/i.test(rarityText[0]) ? "An" : "A";
+  const rarityText = getRarityText(rarity);
+  const locationText = getLocationText(location);
+  const monthsText = getMonthsText(months);
+  const timeText = getTimeText(time);
 
   return (
     <DescriptionBox icon={image_url}>
--- components/FishDescription/index.js
+++ components/FishDescription/index.js
@@ -1,47 +1,30 @@
+import {
+  getLocationText,
+  getMonthsText,
+  getRarityText,
+  getTimeText,
+} from "@/lib/utils";
 import DescriptionBox from "../DescriptionBox";
+
+function getSizeText(shadow_size) {
+  if (shadow_size === "Medium") {
+    return "Medium-sized";
+  } else if (shadow_size === "Very large (finned)") {
+    return "Very large";
+  } else {
+    return shadow_size;
+  }
+}
 
 export default function FishDescription({ fish }) {
   const { shadow_size, location, rarity, image_url } = fish;
   const { months, time } = fish.north.availability_array[0];
 
-  const sizeText =
-    shadow_size === "Medium"
-      ? "Medium-sized"
-      : shadow_size === "Very large (finned)"
-      ? "Very large"
-      : shadow_size;
-
-  const rarityText =
-    rarity === ""
-      ? "common "
-      : rarity === "Unknown"
-      ? null
-      : `${rarity.toLowerCase()} `;
-
-  const locationText =
-    location === "River"
-      ? "in rivers"
-      : location === "Pier"
-      ? "at the Pier"
-      : location === "Pond"
-      ? "in ponds"
-      : location === "River (clifftop)"
-      ? "in clifftop rivers"
-      : location === "Sea (raining)"
-      ? "at Sea when it's raining"
-      : location === "River (mouth)"
-      ? "at river mouths"
-      : "at Sea";
-
-  const monthsText =
-    months === "All year"
-      ? " all\u2011year"
-      : months.includes("; ")
-      ? ` from ${months.replace(/; /g, " & ")}`
-      : ` from ${months}`;
-
-  const timeText = time === "All day" ? "all\u2011day" : time;
-
+  const sizeText = getSizeText(shadow_size);
+  const rarityText = getRarityText(rarity);
+  const locationText = getLocationText(location);
+  const monthsText = getMonthsText(months);
+  const timeText = getTimeText(time);
   const hasFinnedShadow = shadow_size === "Very large (finned)" ? true : false;
 
   return (
