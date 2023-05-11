4011eed855cd96d50715d9de1844e07f2a2f2b17
Xaver Schüll <botacode@gmail.com>
2023-05-11 15:14:27 +0200

    Removed redundant styling

--- components/BackLink/index.js
+++ components/BackLink/index.js
@@ -2,16 +2,10 @@ import styled from "styled-components";
 import Link from "next/link";
 import Button from "../StyledButton";
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: #593b23;
-  align-self: flex-start;
-`;
-
 export default function BackLink() {
   return (
-    <StyledLink href={"/"} aria-label="Navigate back to Island Overview page">
+    <Link href={"/"} aria-label="Navigate back to Island Overview page">
       <Button>&larr; back</Button>
-    </StyledLink>
+    </Link>
   );
 }
