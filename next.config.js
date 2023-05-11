3672d96ea15a2609cc016e00d5d814a8d1b7b6f3
Xaver Schüll <botacode@gmail.com>
2023-05-11 13:21:53 +0200

    Removed unused env, dummy image domain

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
-  env: {
-    API_KEY: process.env.API_KEY,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -18,11 +15,6 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dummyimage.com",
-        port: "",
-      },
       {
         protocol: "https",
         hostname: "acnhapi.com",
